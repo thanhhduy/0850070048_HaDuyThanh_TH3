6e218befddd9633607054af5d13c1913d7cf467c
Thành Hà Duy <0850070048@sv.hcmunre.edu.vn>
2023-08-04 18:40:22 +0700

    Ha Duy Thanh

---
+++ flutterweb/lib/Screens/Home/Components/menu_item.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import '../../../constant.dart';
+
+class MenuItem extends StatelessWidget {
+  final String title;
+  final Function press;
+  const MenuItem({
+    Key? key,
+    required this.title,
+    required this.press,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        // Call the press function here
+        press();
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        child: Text(
+          title.toUpperCase(),
+          style: TextStyle(
+            color: kTextcolor.withOpacity(0.3),
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ shop_app/lib/theme.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import 'constants.dart';
+
+ThemeData theme() {
+  return ThemeData(
+    scaffoldBackgroundColor: Colors.white,
+    fontFamily: "Muli",
+    appBarTheme: appBarTheme(),
+    textTheme: textTheme(),
+    inputDecorationTheme: inputDecorationTheme(),
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+  );
+}
+
+InputDecorationTheme inputDecorationTheme() {
+  OutlineInputBorder outlineInputBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.circular(28),
+    borderSide: const BorderSide(color: kTextColor),
+    gapPadding: 10,
+  );
+  return InputDecorationTheme(
+    floatingLabelBehavior: FloatingLabelBehavior.always,
+    contentPadding: const EdgeInsets.symmetric(horizontal: 42, vertical: 20),
+    enabledBorder: outlineInputBorder,
+    focusedBorder: outlineInputBorder,
+    border: outlineInputBorder,
+  );
+}
+
+TextTheme textTheme() {
+  return const TextTheme(
+    bodyLarge: TextStyle(color: kTextColor),
+    bodyMedium: TextStyle(color: kTextColor),
+    // Add more text styles if needed
+  );
+}
+
+AppBarTheme appBarTheme() {
+  return const AppBarTheme(
+    backgroundColor: Colors.white,
+    elevation: 0,
+    iconTheme: IconThemeData(color: Colors.black),
+  );
+}
+
